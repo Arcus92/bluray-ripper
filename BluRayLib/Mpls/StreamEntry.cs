245634d70543fcd2adcc43817990112ea8d71da6
David Schulte <mail@david-schulte.de>
2025-04-09 21:22:50 +0200

    Clean up StreamEntry.

--- BluRayLib/Mpls/StreamEntry.cs
+++ BluRayLib/Mpls/StreamEntry.cs
@@ -18,24 +18,20 @@ public class StreamEntry
         if (length == 0) return;
         
         Type = (MplsStreamType)reader.ReadByte();
-        int size;
         switch (Type)
         {
             case MplsStreamType.PlayItem:
                 RefToStreamId = reader.ReadUInt16();
-                size = 2;
                 break;
             case MplsStreamType.SubPath1:
                 RefToSubPathId = reader.ReadByte();
                 RefToSubClipId = reader.ReadByte();
                 RefToStreamId = reader.ReadUInt16();
-                size = 4;
                 break;
             case MplsStreamType.SubPath2:
             case MplsStreamType.SubPath3:
                 RefToSubPathId = reader.ReadByte();
                 RefToStreamId = reader.ReadUInt16();
-                size = 3;
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
